265f3cc33e764281fd509c426e8d8df081c1e0f8
RohithNarra08 <narra1602@gmail.com>
2026-02-26 13:40:00 +0530

    Add files via upload

---
+++ 01-user-service/src/main/java/com/codegnan/dto/CredentialDto.java
@@ -0,0 +1,40 @@
+package com.codegnan.dto;
+
+import com.codegnan.models.RoleBasedAuthority;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class CredentialDto {
+
+	private String username;
+	private String password;
+	private RoleBasedAuthority roleBasedAuthority;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public RoleBasedAuthority getRoleBasedAuthority() {
+		return roleBasedAuthority;
+	}
+	public void setRoleBasedAuthority(RoleBasedAuthority roleBasedAuthority) {
+		this.roleBasedAuthority = roleBasedAuthority;
+	}
+	public CredentialDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
